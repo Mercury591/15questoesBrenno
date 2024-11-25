c431d77d52b96d1c94caa86515d3110b02e5d8d5
DaniloS-Lima <danillolima2022@yahoo.com>
2024-11-25 20:08:56 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -30,89 +30,26 @@ class Main {
 
         quiz.executar();
     }
-
-    static class Questao {
-        private String pergunta;
-        private String opcaoA;
-        private String opcaoB;
-        private String opcaoC;
-        private String opcaoD;
-        private String opcaoE;
-        private String correta;
-
-        public Questao(String pergunta, String opcaoA, String opcaoB, String opcaoC, String opcaoD, String opcaoE, String correta) {
-            this.pergunta = pergunta;
-            this.opcaoA = opcaoA;
-            this.opcaoB = opcaoB;
-            this.opcaoC = opcaoC;
-            this.opcaoD = opcaoD;
-            this.opcaoE = opcaoE;
-            this.correta = correta;
-        }
-
-        public void escrevaQuestao() {
-            System.out.println(this.pergunta);
-            System.out.println(this.opcaoA);
-            System.out.println(this.opcaoB);
-            System.out.println(this.opcaoC);
-            System.out.println(this.opcaoD);
-            System.out.println(this.opcaoE);
-            System.out.println();
-        }
-
-        public String leiaResposta() {
-            Scanner ler = new Scanner(System.in);
-            String resp;
-            do {
-                System.out.println("Digite a resposta: ");
-                resp = ler.next();
-            } while (!respostaValida(resp));
-            return resp;
-        }
-
-        private boolean respostaValida(String resp) {
-            if (resp.equalsIgnoreCase("A") ||
-                    resp.equalsIgnoreCase("B") ||
-                    resp.equalsIgnoreCase("C") ||
-                    resp.equalsIgnoreCase("D") ||
-                    resp.equalsIgnoreCase("E")) {
-                return true;
-            }
-            System.out.println("Resposta inválida! digite opção A,B,C,D ou E.");
-            System.out.println();
-            return false;
-        }
-
-        public boolean isCorreta(String resposta) {
-            if (resposta.equalsIgnoreCase(this.correta)) {
-                System.out.println("Parabéns, resposta correta! - Letra: " + this.correta);
-                System.out.println();
-                return true;
-            } else {
-                System.out.println("Resposta errada!");
-                System.out.println("A resposta correta é: " + this.correta);
-                System.out.println();
-                return false;
-            }
-        }
-    }
 }
 
 class Quiz {
-    private List<Main.Questao> questoes;
+    private List<Questao> questoes;
     private int pontuacao;
+
+    private int pontuacaoFinal;
 
     public Quiz() {
         questoes = new ArrayList<>();
         pontuacao = 0;
+        pontuacaoFinal = 0;
     }
 
-    public void adicionarQuestao(Main.Questao questao) {
+    public void adicionarQuestao(Questao questao) {
         questoes.add(questao);
     }
 
     public void executar() {
-        for (Main.Questao questao : questoes) {
+        for (Questao questao : questoes) {
             questao.escrevaQuestao();
             String resposta = questao.leiaResposta();
             if (questao.isCorreta(resposta)) {
@@ -121,6 +58,7 @@ class Quiz {
             System.out.println("Sua pontuação atual é: " + pontuacao);
             System.out.println();
         }
+        System.out.println("A quantidade de acertos em porcentagem é de: " + ((pontuacao/15.0)*100));
         System.out.println("Quiz finalizado! Sua pontuação final é: " + pontuacao);
     }
 }
